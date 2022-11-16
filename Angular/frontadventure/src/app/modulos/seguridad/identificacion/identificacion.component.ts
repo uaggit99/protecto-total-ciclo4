0045d9973b6bb5fa0a534e97697e19e5e8492079
uaggit99 <ace77ey@gmail.com>
2022-11-15 19:28:21 -0500

    ajuste de formulario  inico sesion

--- Angular/frontadventure/src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ Angular/frontadventure/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificacionComponent implements OnInit {
 
-  constructor() { }
+  
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
