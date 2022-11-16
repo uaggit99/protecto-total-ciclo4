0eb7b613b309b6d6c29ab38ec952c6d282847230
uaggit99 <ace77ey@gmail.com>
2022-11-15 21:07:26 -0500

    funcion identificar usuario

--- Angular/frontadventure/src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ Angular/frontadventure/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificacion',
@@ -8,11 +8,22 @@ import { FormBuilder } from '@angular/forms';
 })
 export class IdentificacionComponent implements OnInit {
 
-  
+  fgValidador : FormGroup = this.fb.group({
+    "usuario":['',[Validators.required, Validators.email]],
+    "clave" :['',[Validators.required]]
+
+  });
   
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+  }
+  identificarUsuario(){
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+    alert(usuario)
+    alert(clave)
   }
 
 }
